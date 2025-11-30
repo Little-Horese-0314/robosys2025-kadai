434ebb37c1d26ca1190e20422e6b78ccf8bbb474
Little Horse <s24c1086wz@s.chibakoudai.jp>
2025-11-30 11:19:34 +0900

    Add average

--- test.bash
+++ test.bash
@@ -1,28 +1,22 @@
-#!/bin/bash -xv  
-
+#!/bin/bash -xv
 
 ng () {
-	echo ${1}行目が違うよ
-       res=1	
+    echo "$1 行目が違うよ"
+    res=1
 }
-
-
 
 res=0
 
-out=$(seq 5.0 | ./plus)
-[ "${out}" = 15.0 ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+[ "${out}" = 3.0 ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo あ 1 2 | ./plus)
+[ "${out}" = 1.5 ] || ng "$LINENO"  
+
 
 [ "${res}" = 0 ] && echo OK
 
-
-
 exit $res
+
